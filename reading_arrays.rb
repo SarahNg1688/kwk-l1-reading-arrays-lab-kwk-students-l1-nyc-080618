f13e68703e57edc90419f81ec3553532e6101fbb
Sarah Ng <agbtr101a@gmail.com>
2018-08-07 20:26:05 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -12,7 +12,7 @@ def first_student_by_index
 end
 
 def fourth_student_by_index
-  # Write a solution that returns the first student in the array student_names
+  #nt_names
 end
 
 def last_student_by_index
